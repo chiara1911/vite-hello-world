220e8e26666309687753defb56a374965047b75f
chiara1911 <chiaracesari89@gmail.com>
2023-11-12 12:19:56 +0100

    add image

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 
 
 <template>
-  
+  <AppHero />
   <AppTitle />
 </template>
 
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   name: 'App',
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   }
 }
 </script>
--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,14 +1,25 @@
 <template>
-    <div>
+  <div class="container">
+    <div class="hero">      
   </div>
+</div>
 </template>
 
 <script>
     export default {
-        
+        name: 'AppHero'
     }
 </script>
 
 <style lang="scss" scoped>
 
+.hero{
+  background-image: url(/images/helloWorld.avif);
+  object-fit: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 400px;
+  display: block;
+
+}
 </style>
